cef5ccaf4dc50c16ac2e484873ee6d734e108717
Rytech <rytech16@gmail.com>
2024-01-31 05:57:12 -0600

    Follow Up Update For Merge rAthena - 2024-01-26
    =General
    *Updated some job related files in the 4CrAM database.
    -This is a follow up to the last update.
    
    *Removed the Hyper Novice skill tree from the RE skill_tree.yml.
    -Well be using the one in the 4CrAM database instead since I have everything in ID order.
    
    *Updated the displayskillunit command.
    -Now properly takes in the player ID to help display things like Mirage.
    -Also now uses a random entity ID on each use to fix the unit stacking issue.

--- src/map/clif.cpp
+++ src/map/clif.cpp
@@ -5525,8 +5525,8 @@ void clif_skill_unit_test(struct block_list *bl, short x, short y, int unit_id, 
 
 	WBUFW(buf, 0) = 0x09ca;
 	WBUFW(buf, 2) = packet_len(0x09ca);
-	WBUFL(buf, 4) = bl->id;
-	WBUFL(buf, 8) = 2000;
+	WBUFL(buf, 4) = rnd_value(2000, 2999);
+	WBUFL(buf, 8) = bl->id;
 	WBUFW(buf, 12) = x;
 	WBUFW(buf, 14) = y;
 	WBUFL(buf, 16) = unit_id;
