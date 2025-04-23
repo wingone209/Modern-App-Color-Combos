209abf8ab3de749f82721fecffc9a9052f326aa2
Rytech <rytech16@gmail.com>
2025-04-22 23:44:30 -0500

    rAthena Merge 2025-04-22 Part 5
    Merged rAthena - 2025-01-23
    Name: Rebalance Sky emperor 2024.02.05 (#8956)
    Hash: b55c78733c6138361e9e5efa1f0e868f3a3cb9b1
    
    Note: Source merge only. Other files will be merged in at the end.

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -4296,8 +4296,10 @@ static void battle_calc_skill_base_damage(struct Damage* wd, struct block_list *
 				}
 #else
 				if ((skill = pc_checkskill(sd, TK_POWER)) > 0) {
-					ATK_ADDRATE(wd->damage, wd->damage2, 10 + 15 * skill);
-					RE_ALLATK_ADDRATE(wd, 10 + 15 * skill);
+					int32 dmg_bonus = 20 * skill;
+
+					ATK_ADDRATE(wd->damage, wd->damage2, dmg_bonus);
+					RE_ALLATK_ADDRATE(wd, dmg_bonus);
 				}
 #endif
 			}
@@ -6867,6 +6869,12 @@ static void battle_attack_sc_bonus(struct Damage* wd, struct block_list *src, st
 			skillratio += sstatus->str; // SG_STAR_ANGER additionally has STR added in its formula.
 		if (anger_level < 4)
 			skillratio /= 12 - 3 * anger_level;
+
+#ifdef RENEWAL
+		// (renewal) maximum damage bonus limit : (skill level x 25)%
+		skillratio = min( skillratio, 25 * anger_level );
+#endif
+
 		ATK_ADDRATE(wd->damage, wd->damage2, skillratio);
 #ifdef RENEWAL
 		RE_ALLATK_ADDRATE(wd, skillratio);
